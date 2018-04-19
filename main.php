51f68a5d2f3f37dbddee8c730d9581cb93aa0e3b
mohit-gh <mohitjajani85@gmail.com>
2018-04-19 12:12:39 +0530

    Update main.php

--- main.php
+++ main.php
@@ -1,7 +1,9 @@
 <?php
 date_default_timezone_set('Asia/Kolkata');
 echo date("d-M-y")."<br>";    //07-Apr-18
+echo date('l D-M-Y H:i:s A'); //Thursday Thu-Apr-2018 12:07:59 PM
 echo date("h:i:sa")."<br>";   //04:49:46pm
+echo date('D-M-Y', strtotime('18 sept 2018')); //Tue-Sep-2018 (Maximum three characters allowed in month parameter)
 echo date('d-m-Y', strtotime("Monday"))."<br/>";    //09-04-2018
 echo date('d/m/Y', strtotime("+3 Weeks"))."<br/>";  //28/04/2018
 //strtotime with two parameters
