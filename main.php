af3b9f57854f91813828995457b322538f554e22
mohit-gh <mohitjajani85@gmail.com>
2018-04-23 11:57:43 +0530

    Update main.php

--- main.php
+++ main.php
@@ -9,6 +9,7 @@ echo date('d/m/Y', strtotime("+3 Weeks"))."<br/>";  //28/04/2018
 //strtotime with two parameters
 $next_day = strtotime("Monday");
 echo date('d-M-Y', strtotime("+10 days", $next_day))."<br/>";   //19-Apr-2018
+echo date('d-M-Y', strtotime('-9 days',strtotime('monday') ) ); //14-Apr-2018 (Monday is 23-Apr-2018)
 $bd = mktime(03,30,9,05,8,1993);
 echo "MK-Time ".date("d-M-y h:i:sa", $bd)."<br/>";    //MK-Time 08-May-93 03:30:09am
 echo date('d-m-Y', strtotime("12-08-2017 -1 year"));  //12-08-2016
